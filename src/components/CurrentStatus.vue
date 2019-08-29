d10ab638811dcbf5e531ff8c94390c0313322552
ibuky@VTX0 <ivuki.maxx@gmail.com>
2019-08-30 01:28:42 +0900

    add more params

--- src/components/CurrentStatus.vue
+++ src/components/CurrentStatus.vue
@@ -1,14 +1,22 @@
 <template>
   <nav class="navbar is-transparent is-fixed-bottom">
-    <p>タイプ数: {{ countTyped }}</p>
-    <p>ミス数: {{ countMiss }}</p>
-    <p>完了数: {{ countComp }}</p>
+    <p>タイプ数: {{ countTyped }} </p>&nbsp;
+    <p>ミス数: {{ countMiss }} </p>&nbsp;
+    <p>完了数: {{ countComp }} </p>&nbsp;
+    <p>残数: {{ countLastSentence }} </p>&nbsp;
+    <p>総数: {{ countStartSentence }} </p>&nbsp;
   </nav>
 </template>
 
 <script>
 export default {
   name: 'CurrentStatus',
-  props: ['countTyped', 'countMiss', 'countComp']
+  props: [
+    'countTyped',
+    'countMiss',
+    'countComp',
+    'countStartSentence',
+    'countLastSentence'
+  ]
 }
 </script>
