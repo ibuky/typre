333681e33ccaae1c49f1a98c48e40d61ef28a11a
ibuky@VTX0 <ivuki.maxx@gmail.com>
2019-08-30 01:28:33 +0900

    add result page

---
+++ src/components/Result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="typing-test">
+    <section class="hero is-fullheight-with-navbar">
+      <div class="hero-body">
+        <div class="container">
+          <div class="is-12 has-background-black result-bg">
+            <p class="has-text-white">test</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+  .result-bg {
+    padding: 100px;
+    border-radius: 6px;
+  }
+</style>
