5cd4a9775c1a4ca345653f5050988f36f9698d44
ibuky@VTX0 <ivuki.maxx@gmail.com>
2019-10-14 15:28:46 +0900

    add wikijs

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -65,7 +65,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         ignore: ['.*']
       }
     ])
-  ]
+  ],
+  externals:[{
+    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}',
+    "isomorphic-fetch": "fetch"
+  }],
 })
 
 module.exports = new Promise((resolve, reject) => {
